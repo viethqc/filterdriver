4cc5db7f9c70efc7934121d8a295908df78928ec
viethq <hoangvietbk2.06@gmail.com>
2018-12-21 01:17:58 +0700

    filter IRP_MJ_DIRECTORY_CONTROL

--- FsFilter/FsFilter/FsFilter.c
+++ FsFilter/FsFilter/FsFilter.c
@@ -52,6 +52,14 @@ _In_ PCFLT_RELATED_OBJECTS FltObjects,
 _Flt_CompletionContext_Outptr_ PVOID *CompletionContext
 );
 
+FLT_POSTOP_CALLBACK_STATUS
+FsFilterPostDirectoryControOperation(
+_Inout_ PFLT_CALLBACK_DATA Data,
+_In_ PCFLT_RELATED_OBJECTS FltObjects,
+_In_opt_ PVOID CompletionContext,
+_In_ FLT_POST_OPERATION_FLAGS Flags
+);
+
 
 #ifdef ALLOC_PRAGMA
 #pragma alloc_text(INIT, DriverEntry)
@@ -71,7 +79,7 @@ CONST FLT_OPERATION_REGISTRATION Callbacks[] = {
 	{ IRP_MJ_DIRECTORY_CONTROL,
 	  0,
 	  FsFilterPreDirectoryControOperation,
-	  NULL, },
+	  FsFilterPostDirectoryControOperation, },
 
     { IRP_MJ_OPERATION_END }
 };
@@ -206,19 +214,183 @@ _Flt_CompletionContext_Outptr_ PVOID *CompletionContext
 {
 	UNREFERENCED_PARAMETER(FltObjects);
 	UNREFERENCED_PARAMETER(CompletionContext);
-/*
+
+	/*PFLT_FILE_NAME_INFORMATION FileNameInfo;
+	NTSTATUS status;
 	WCHAR szFileName[512 + 1] = { 0 };
+	PFILE_BOTH_DIR_INFORMATION	stDirInfo;
+	PFILE_BOTH_DIR_INFORMATION	dir_info;
+	UNICODE_STRING	EntryName;
 
-	RtlStringCchCopyNW(szFileName, 512, FltObjects->FileObject->FileName.Buffer, FltObjects->FileObject->FileName.Length);*/
-
-	DbgPrint("FsFilterPreDirectoryControOperation\n");
-	/*if (wcsstr(szFileName, L"test.txt") != NULL)
+	status = FltGetFileNameInformation(Data, FLT_FILE_NAME_NORMALIZED | FLT_FILE_NAME_QUERY_DEFAULT, &FileNameInfo);
+	if (NT_SUCCESS(status))
 	{
-		DbgPrint("File not create\n");
-		return FLT_PREOP_DISALLOW_FASTIO;
+		status = FltParseFileNameInformation(FileNameInfo);
+
+		if (NT_SUCCESS(status))
+		{
+			if (FileNameInfo->Name.MaximumLength < 260)
+			{
+				RtlCopyMemory(szFileName, FileNameInfo->Name.Buffer, FileNameInfo->Name.MaximumLength);
+				_wcsupr(szFileName);
+
+				DbgPrint("FsFilterPreDirectoryControOperation: %wS\n", szFileName);
+			}
+		}
+
+		FltReleaseFileNameInformation(FileNameInfo);
 	}*/
 
 	return FLT_PREOP_SUCCESS_WITH_CALLBACK;
+}
+
+FLT_POSTOP_CALLBACK_STATUS
+FsFilterPostDirectoryControOperation(
+_Inout_ PFLT_CALLBACK_DATA Data,
+_In_ PCFLT_RELATED_OBJECTS FltObjects,
+_In_opt_ PVOID CompletionContext,
+_In_ FLT_POST_OPERATION_FLAGS Flags
+)
+{
+	PFLT_FILE_NAME_INFORMATION FileNameInfo;
+	NTSTATUS status;
+	WCHAR szFileName[512 + 1] = { 0 };
+	PFILE_BOTH_DIR_INFORMATION	stDirInfo;
+	PFILE_BOTH_DIR_INFORMATION	dir_info;
+	UNICODE_STRING	EntryName;
+	UNICODE_STRING uStrParent;
+	NTSTATUS rc;
+	PUNICODE_STRING puStr = NULL;
+	PVOID FileInformation = NULL;
+	UNICODE_STRING defaultName;
+	UNICODE_STRING uStr, uStr1;
+	PUNICODE_STRING nameToUse;
+	PFLT_FILE_NAME_INFORMATION nameInfo = NULL;
+	char * ptrParentDir = NULL;
+	PCHAR ptr = NULL;
+	char * buffer = NULL;
+	wchar_t wcFullPathName[1024];
+	int BytesReturned = 0;
+	int bytesreturned = 0;
+	int i = 0, iPos = 0;
+	int j = 0, iLeft = 0;
+	BOOLEAN bDone;
+
+	RtlInitUnicodeString(&uStr, L"b.txt");
+
+	status = FltGetFileNameInformation(Data, FLT_FILE_NAME_NORMALIZED | FLT_FILE_NAME_QUERY_DEFAULT, &FileNameInfo);
+	if (NT_SUCCESS(status))
+	{
+		status = FltParseFileNameInformation(FileNameInfo);
+
+		if (NT_SUCCESS(status))
+		{
+			if (FileNameInfo->Name.MaximumLength < 260)
+			{
+				RtlCopyMemory(szFileName, FileNameInfo->Name.Buffer, FileNameInfo->Name.MaximumLength);
+				_wcsupr(szFileName);
+
+				if (wcsstr(szFileName, L"DM") == NULL)
+				{
+					FltReleaseFileNameInformation(FileNameInfo);
+					return FLT_POSTOP_FINISHED_PROCESSING;
+				}
+
+
+				DbgPrint("===========================FsFilterPostDirectoryControOperation: %wS===============\n", szFileName);
+				stDirInfo = (PFILE_BOTH_DIR_INFORMATION)Data->Iopb->Parameters.DirectoryControl.QueryDirectory.DirectoryBuffer;
+				ProbeForRead(&stDirInfo->FileName[0], sizeof(WCHAR), 1);
+				DbgPrint("FileName : %ws\n", &stDirInfo->FileName[0]);
+
+				do
+				{
+					stDirInfo = (PFILE_BOTH_DIR_INFORMATION)(((PUCHAR)stDirInfo) + stDirInfo->NextEntryOffset);
+					ProbeForRead(&stDirInfo->FileName[0], sizeof(WCHAR), 1);
+					DbgPrint("FileName : %ws\n", &stDirInfo->FileName[0]);
+
+					stDirInfo->NextEntryOffset = 0;
+				} while (stDirInfo->NextEntryOffset != 0);
+
+				/*if (stDirInfo->NextEntryOffset == 0) {
+					DbgPrint("NextEntryOffset = 0");
+				}
+				else
+				{
+					DbgPrint("Next offset is not zero\n");
+					stDirInfo = (PFILE_BOTH_DIR_INFORMATION)(((PUCHAR)stDirInfo) + stDirInfo->NextEntryOffset);
+					ProbeForRead(&stDirInfo->FileName[0], sizeof(WCHAR), 1);
+					DbgPrint("2 : %ws\n", &stDirInfo->FileName[0]);
+
+					if (stDirInfo->NextEntryOffset == 0) {
+						DbgPrint("NextEntryOffset = 0");
+					}
+					else
+					{
+						DbgPrint("Next offset is not zero\n");
+						stDirInfo = (PFILE_BOTH_DIR_INFORMATION)(((PUCHAR)stDirInfo) + stDirInfo->NextEntryOffset);
+						ProbeForRead(&stDirInfo->FileName[0], sizeof(WCHAR), 1);
+						DbgPrint("3 : %ws\n", &stDirInfo->FileName[0]);
+					}
+				}*/
+
+				//bytesreturned = 0;
+
+				//while (1) {
+
+				//	ProbeForRead(&stDirInfo->FileName[0], sizeof(WCHAR), 1);
+
+				//	DbgPrint("File Name : %ws\n", &stDirInfo->FileName[0]);
+				//	uStr1.Length = (USHORT)stDirInfo->FileNameLength;
+				//	uStr1.MaximumLength = uStr1.Length;
+				//	uStr1.Buffer = &stDirInfo->FileName[0];
+
+				//	//RtlInitUnicodeString(&uStr1,&stDirInfo->FileName[0]);
+
+				//	/*if (RtlCompareUnicodeString(&uStr1, &uStr, TRUE) == 0)
+				//	{
+				//		DbgPrint("File Compared");
+				//		if (stDirInfo->NextEntryOffset == 0)
+				//			break;
+
+				//		stDirInfo = (PFILE_BOTH_DIR_INFORMATION)(((PUCHAR)stDirInfo) + stDirInfo->NextEntryOffset);
+				//		continue;
+				//	}*/
+
+				//	if (stDirInfo->NextEntryOffset != 0) {
+
+				//		bytesreturned += stDirInfo->NextEntryOffset;
+				//		DbgPrint("BytesRetured = %d, Offset Not Zero = %d\ n ", bytesreturned, stDirInfo->NextEntryOffset);
+				//	}
+				//	else {
+				//		bytesreturned += sizeof(*stDirInfo) - sizeof(WCHAR)+stDirInfo->FileNameLength;
+				//		DbgPrint("BytesRetured = %d, Offset = %d\ n ", bytesreturned, stDirInfo->NextEntryOffset);
+				//	}
+				//	//ptr+=stDirInfo->NextEntryOffset;
+				//	//DbgPrint("ptr = %d\n",(ULONG)buffer);
+
+				//	if (stDirInfo->NextEntryOffset == 0) {
+				//		DbgPrint("Quit from inner loop");
+				//		break;
+				//	}
+
+				//	stDirInfo = (PFILE_BOTH_DIR_INFORMATION)(((PUCHAR)stDirInfo) + stDirInfo->NextEntryOffset);
+				//	DbgPrint("Next offset is not zero\n");
+
+				//} // while end
+
+				//if (bytesreturned > 0) {
+				//	DbgPrint("BytesRetured > = %d\n", bytesreturned);
+				//}
+				//Data->IoStatus.Information = bytesreturned;
+
+				//RtlCopyMemory((PVOID)Data->Iopb - > Parameters.DirectoryControl.QueryDirectory.DirectoryBuffer, (PVOID)stDirInfo, bytesreturned);
+			}
+		}
+
+		FltReleaseFileNameInformation(FileNameInfo);
+	}
+
+	return FLT_POSTOP_FINISHED_PROCESSING;
 }
 
 
@@ -246,13 +418,13 @@ _In_ PCFLT_RELATED_OBJECTS FltObjects,
 _Flt_CompletionContext_Outptr_ PVOID *CompletionContext
 	)
 {
-	DbgPrint("FsFilterPreSetOperation\n");
+	//DbgPrint("FsFilterPreSetOperation\n");
 
 	WCHAR szFileName[512 + 1] = { 0 };
 
 	RtlStringCchCopyNW(szFileName, 512, FltObjects->FileObject->FileName.Buffer, FltObjects->FileObject->FileName.Length);
 
-	DbgPrint(L"FsFilterPreSetOperation: %ws\n", szFileName);
+	//DbgPrint(L"FsFilterPreSetOperation: %ws\n", szFileName);
 	if (wcsstr(szFileName, L"FsFilter.sys") != NULL)
 	{
 		return FLT_PREOP_DISALLOW_FASTIO;
---
+++ dm/dm/dm.cpp
@@ -0,0 +1,73 @@
+// dm.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "dm.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// The one and only application object
+
+CWinApp theApp;
+
+using namespace std;
+
+int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
+{
+	int nRetCode = 0;
+
+	HMODULE hModule = ::GetModuleHandle(NULL);
+
+	if (hModule != NULL)
+	{
+		// initialize MFC and print and error on failure
+		if (!AfxWinInit(hModule, NULL, ::GetCommandLine(), 0))
+		{
+			// TODO: change error code to suit your needs
+			_tprintf(_T("Fatal Error: MFC initialization failed\n"));
+			nRetCode = 1;
+		}
+		else
+		{
+			// TODO: code your application's behavior here.
+			//if (PathFileExists(L"C:\\a.txt") == FALSE)
+			//{
+			//	_tprintf(_T("File not found\n"));
+			//}
+
+			//DWORD ftyp = GetFileAttributes(L"C:\\a.txt");
+			//if (ftyp == INVALID_FILE_ATTRIBUTES)
+			//{
+			//	_tprintf(_T("Get file attribute error\n"));
+			//}
+
+			HANDLE hFind = INVALID_HANDLE_VALUE;
+			WIN32_FIND_DATA find_data;
+			BOOL bFind;
+
+			hFind = FindFirstFile(_T("C:\\dm\\*.*"), &find_data);
+			if (hFind == INVALID_HANDLE_VALUE)
+			{
+				_tprintf(_T("Find first file error\n"));
+				return 0;
+			}
+
+			_tprintf(_T("%s\n"), find_data.cFileName);
+			while (bFind = FindNextFile(hFind, &find_data))
+			{
+				_tprintf(_T("%s\n"), find_data.cFileName);
+			}
+		}
+	}
+	else
+	{
+		// TODO: change error code to suit your needs
+		_tprintf(_T("Fatal Error: GetModuleHandle failed\n"));
+		nRetCode = 1;
+	}
+
+	return nRetCode;
+}
